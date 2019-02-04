732f125f7c8d70d1fbab06868b5165320651370a
Denis Moldavskiy <damoldavskiy@edu.hse.ru>
2019-02-04 17:54:08 +0300

    Splash screen added

--- NotepadOnlineMobile/NotepadOnlineMobile.Android/MainActivity.cs
+++ NotepadOnlineMobile/NotepadOnlineMobile.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace NotepadOnlineMobile.Droid
 {
-    [Activity(Label = "Notepad.Online Mobile", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Notepad.Online Mobile", Icon = "@drawable/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ NotepadOnlineMobile/NotepadOnlineMobile.Android/SplashActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace NotepadOnlineMobile.Droid
+{
+    [Activity(Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed()
+        { }
+    }
+}
